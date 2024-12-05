3dcf7f6ce645ca0a3d88937958d80dd02222521f
Barasa-Mercyline <barasamercyline226@gmail.com>
2024-12-05 14:52:49 +0300

    mapping data to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,15 @@ import Note from "./Note";
 import Card from "./Card";
 import contacts from "../contacts";
 
+function createCard(contact){
+       return <Card 
+       key={contact.id}
+       name={contact.name}
+       img={contact.imgURL}
+       tel={contact.phone}
+       email={contact.email} />
+}
+
 function App(){
     return (
     <div >
@@ -12,8 +21,9 @@ function App(){
         <Note/>
         <h1 className="heading">My Contacts</h1>
           
+          {contacts.map(createCard)};
           
-        <Card       
+        {/* <Card       
                name={contacts[0].name}     
                img={contacts[0].imgURL}       
                tel={contacts[0].phone}    
@@ -37,7 +47,7 @@ function App(){
                img={contacts[3].imgURL}       
                tel={contacts[3].phone}    
                email={contacts[3].email}
-        />
+        /> */}
   
         <Footer />
         </div>)
