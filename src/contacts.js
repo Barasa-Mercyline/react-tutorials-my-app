db22df89a3b10439b5c3dd72fa3c05305b82306e
Barasa-Mercyline <barasamercyline226@gmail.com>
2024-12-05 12:57:40 +0300

    card

---
+++ src/contacts.js
@@ -0,0 +1,33 @@
+const contacts = [
+   
+  {
+    name: "Mercyline",
+    imgURL: "https://drive.google.com/file/d/14-b-60jVF4LB7eqlf4x2F3Cvge4uriza/view?usp=drive_link.jpg",
+    phone: "+2547144133",
+    email: "m@cy.com ",
+  },
+
+  { 
+    name: "Beyonce",
+    imgURL:
+      "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
+    phone: "+123 456 789",
+    email: "b@beyonce.com",
+  },
+  {
+    name: "Jack Bauer",
+    imgURL:
+      "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
+    phone: "+987 654 321",
+    email: "jack@nowhere.com",
+  },
+  {
+    name: "Chuck Norris",
+    imgURL:
+      "https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png",
+    phone: "+918 372 574",
+    email: "gmail@chucknorris.com",
+  },
+];
+
+export default contacts;
